3ab5a7c63289c861feb6fd3b7afc0e36bc79cbbc
poe <poe1599@gmail.com>
2021-02-03 01:50:29 +0800

    0203 poe cart SM-Coupon頁面

--- src/components/CartSimpleMealCoupon.js
+++ src/components/CartSimpleMealCoupon.js
@@ -4,6 +4,52 @@ import { withRouter, Link } from 'react-router-dom'
 function CartSimpleMealCoupon() {
   // 方案選擇
   const [choice, setChoice] = useState('')
+  const [choiceObj, setChoiceObj] = useState({
+    couponNum: 0,
+    price: 0,
+  })
+  // 方案內容
+  const choiceArray = [
+    { couponNum: 0, price: 0 },
+    { couponNum: 25, price: 3500 },
+    { couponNum: 12, price: 1700 },
+    { couponNum: 6, price: 850 },
+  ]
+  // 組合數量
+  const [quantity, setQuantity] = useState(0)
+
+  // 改變方案
+  const handleSetChoice = (event) => {
+    setChoice(event.target.value)
+    setQuantity(quantity || 1)
+    handleSetChoiceObj(event.target.value, quantity || 1)
+  }
+
+  // 改變組合文字
+  function handleSetChoiceObj(choice, quantity) {
+    const newCouponNum = choiceArray[+choice].couponNum * quantity
+    const newPrice = choiceArray[+choice].price * quantity
+    setChoiceObj({
+      couponNum: newCouponNum,
+      price: newPrice,
+    })
+  }
+
+  // 組合數量按鈕(true +1; false -1)
+  const handleSetQuantity = (bool) => {
+    if (bool) {
+      if (quantity < 10) {
+        setQuantity(quantity + 1)
+        handleSetChoiceObj(choice, quantity + 1)
+      }
+    } else {
+      if (quantity >= 2) {
+        setQuantity(quantity - 1)
+        handleSetChoiceObj(choice, quantity - 1)
+      }
+    }
+  }
+
   return (
     <>
       <h5>選購方案</h5>
@@ -23,7 +69,14 @@ function CartSimpleMealCoupon() {
         <tbody>
           <tr>
             <th scope="row">
-              <input type="radio" name="poePlan" />
+              <input
+                type="radio"
+                value="1"
+                checked={choice === '1'}
+                onChange={(e) => {
+                  handleSetChoice(e)
+                }}
+              />
             </th>
             <td>
               <span>吃飽飽沒煩惱組合</span>
@@ -41,7 +94,14 @@ function CartSimpleMealCoupon() {
           </tr>
           <tr>
             <th scope="row">
-              <input type="radio" name="poePlan" />
+              <input
+                type="radio"
+                value="2"
+                checked={choice === '2'}
+                onChange={(e) => {
+                  handleSetChoice(e)
+                }}
+              />
             </th>
             <td>
               <span>食在很滿足組合</span>
@@ -59,7 +119,14 @@ function CartSimpleMealCoupon() {
           </tr>
           <tr>
             <th scope="row">
-              <input type="radio" name="poePlan" />
+              <input
+                type="radio"
+                value="3"
+                checked={choice === '3'}
+                onChange={(e) => {
+                  handleSetChoice(e)
+                }}
+              />
             </th>
             <td>
               <span>經濟實惠組</span>
@@ -79,33 +146,37 @@ function CartSimpleMealCoupon() {
       </table>
       <div className="poe-bookmark-content-choice text-right">
         <span className="">組合數量</span>
-        <Link href="" className="mx-3">
-          <i className="fas fa-minus-circle"></i>
+        <Link onClick={() => handleSetQuantity(false)} className="mx-3">
+          <i
+            className={`fas fa-minus-circle ${quantity <= 1 && 'poe-gray'}`}
+          ></i>
         </Link>
-        <span>2</span>
-        <Link href="" className="mx-3">
-          <i className="fas fa-plus-circle"></i>
+        <span>{quantity}</span>
+        <Link onClick={() => handleSetQuantity(true)} className="mx-3">
+          <i
+            className={`fas fa-plus-circle ${quantity === 10 && 'poe-gray'}`}
+          ></i>
         </Link>
       </div>
       <div className="poe-bookmark-content-result text-right">
         <div>
           <span>共 </span>
-          <span className="poe-red">2</span>
+          <span className="poe-red">{quantity}</span>
           <span> 組, 包含 </span>
-          <span className="poe-red">50</span>
+          <span className="poe-red">{choiceObj.couponNum}</span>
           <span>
             {' '}
             張餐券, <br className="d-block d-sm-none" />
             金額小計NT${' '}
           </span>
-          <span className="poe-red">7,000</span>
+          <span className="poe-red">{choiceObj.price}</span>
           <span> 元</span>
         </div>
       </div>
       <div className="text-right poe-bookmark-btn-group">
-        <a href="./cart-meal-coupon-payment.html" className="btn-green txt-btn">
+        <Link className="btn-green txt-btn">
           下一步　<i className="fas fa-chevron-right"></i>
-        </a>
+        </Link>
       </div>
     </>
   )
