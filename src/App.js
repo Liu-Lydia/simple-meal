ccf0771b1b828483306bbb1b288666d2e53f5961
poe <poe1599@gmail.com>
2021-02-04 09:36:36 +0800

    0204 日曆merge

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import React, { useEffect, useState } from 'react'
 import Test from './pages/Test'
 import TestDatabase from './pages/TestDatabase'
 import Cart from './pages/Cart'
-<<<<<<< HEAD
 import Long1 from './pages/Long1'
 import Long2 from './pages/Long2'
-=======
-import Calendar from './pages/Calendar'
->>>>>>> a051b8548d6d2e477a61e69ee54956634a85fc30
+// import Calendar from './pages/Calendar'
 
 // 組合用元件
 import MainContent from './components/MainContent'
