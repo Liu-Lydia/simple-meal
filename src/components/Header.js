ec44bae1cb42ca5b1dc1e4a719d7a7dba2403268
Leap Hsu <ailiasdoll@gmail.com>
2021-02-08 17:25:03 +0800

    add link

--- src/components/Header.js
+++ src/components/Header.js
@@ -134,7 +134,7 @@ function Header() {
                     ></HeaderIcon>
                     <HeaderIcon
                       text={'我的成就'}
-                      url={'/milestone'}
+                      url={'/Milestone'}
                       fa={'fas fa-medal'}
                     ></HeaderIcon>
                     <HeaderIcon
