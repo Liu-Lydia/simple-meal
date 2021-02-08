0f9e005e0599658591cf8baf1e5ead7ff322184f
Leap Hsu <ailiasdoll@gmail.com>
2021-02-08 16:42:44 +0800

    some updates on rewardpage

--- src/components/Milestone/ExchangeOption.js
+++ src/components/Milestone/ExchangeOption.js
@@ -35,24 +35,31 @@ function ExchangeOption(props) {
     <>
       <div className="row justify-content-center text-center">
         {/* 分類文字*/}
-        <div className="col-10 col-sm-8 d-flex justify-content-center text-center fff-txt-ms-select">
-          <a id="tab0" className="col-xl-2 col-lg-3 d-flex justify-content-center fff-txt-rw-unselect" onClick={() => { 
+        <div className="col-10 col-sm-11 col-lg-10 col-xl-10 d-flex justify-content-center text-center fff-txt-ms-select">
+
+
+          <a id="tab0" className="col-xl-3 col-md-4 col-sm-4 d-flex justify-content-center fff-txt-rw-unselect" onClick={() => { 
               props.setOptionTab(0)}}>合作<p className="fff-ms-web">廠商</p>優惠</a>
+
+
           <a  id="tab1"
-            className="col-xl-2 col-lg-3 d-flex justify-content-center fff-txt-rw-select-active" onClick={() => { 
+            className="col-xl-3 col-md-4 col-sm-4 d-flex justify-content-center fff-txt-rw-select-active" onClick={() => { 
                 props.setOptionTab(1)}}
           >
             &nbsp;購物金<p className="fff-ms-web">回饋</p>&nbsp;
           </a>
+
+
+
           <a  id="tab2"
-            className="col-xl-2 col-lg-3 fff-txt-rw-unselect" onClick={() => { 
+            className="col-xl-3 col-md-4 col-sm-4 fff-txt-rw-unselect" onClick={() => { 
                 props.setOptionTab(2)}}>
             兌換紀錄
           </a>
         </div>
 
         {/* 那條Bar */}
-        <div className="col-10 col-sm-8 d-flex justify-content-center text-center align-items-center fff-rw-select-line">
+        <div className="col-10 col-sm-11 col-lg-10 col-xl-10 d-flex justify-content-center text-center align-items-center fff-rw-select-line">
           <div className="col-xl-2 col-lg-3 d-flex justify-content-center">
             <div id="bar0" className="col-9 "></div>
           </div>
@@ -63,6 +70,7 @@ function ExchangeOption(props) {
             <div id="bar2" className="col-9 "></div>
           </div>
         </div>
+        <p className="fff-exchange-record-warning">僅顯示一週內兌換資料，詳細兌換資料請拜訪<a href>會員中心</a>查詢</p>
       </div>
 
       
