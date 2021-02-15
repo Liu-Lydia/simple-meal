b5085c1ee73f44050a4bec2d809e67850fec2af1
lin li hsin <xxxamfa@gmail.com>
2021-02-16 01:26:43 +0800

    測試

---
+++ src/data/index.js
@@ -0,0 +1,103 @@
+const data = [
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+  {
+    id: 107001,
+    name: '青檸嫩雞直管麵',
+    introduction:
+      '沒有什麼組合比香腸和辣椒更令人滿意了。畢竟，在胃液中甜美的焦糖味和肉中的超鹹味，還有什麼比這更令人放鬆呢？',
+    cook_time: 40,
+    pic:
+      'http://localhost:3015/img/sharerecipe/5fea5b5d27c560013957dcf9-b9960379.jpg',
+  },
+]
+
+export { data }
