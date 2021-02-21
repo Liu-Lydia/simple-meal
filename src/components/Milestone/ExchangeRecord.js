b78df78025a31dbfa79bd2b9defd90d5564bda1d
Leap Hsu <ailiasdoll@gmail.com>
2021-02-21 14:16:17 +0800

    顯示兌換紀錄

--- src/components/Milestone/ExchangeRecord.js
+++ src/components/Milestone/ExchangeRecord.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../styles/public.css'
 import '../../styles/fff.css'
 
 function ExchangeRecord() {
+  const [exchangeRecord, setExchangeRecord] = useState([])
+  const getExchangeRecord = () => {
+    const url = 'http://localhost:4000/reward/getExchangeRecord?sid=1' //sid 要從session來
+    fetch(url, {
+      method: 'get',
+    })
+      //then 是會接前方拋出的結果
+      .then((r) => r.json())
+      .then((obj) => {
+        setExchangeRecord(obj)
+      })
+  }
+
+  useEffect(() => {
+    getExchangeRecord()
+  }, [])
+
   return (
     <>
       <div className="row fff-no-mr-and-pad d-none d-md-flex">
@@ -20,56 +37,22 @@ function ExchangeRecord() {
                 <th>使用狀況</th>
                 <th>優惠條碼</th>
               </tr>
-              <tr>
-                <td>1</td>
-                <td>50元折價卷</td>
-                <td>2020/01/15</td>
-                <td>2020/02/15</td>
-                <td className="fff-reward-used">已使用</td>
-                <td>
-                  <div className="record">檢視</div>
-                </td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>星巴克買一送一</td>
-                <td>2020/01/15</td>
-                <td>2020/02/15</td>
-                <td className="fff-reward-unuse">可使用</td>
-                <td>
-                  <div className="record">檢視</div>
-                </td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>星巴克買一送一</td>
-                <td>2020/01/15</td>
-                <td>2020/02/15</td>
-                <td className="fff-reward-unuse">可使用</td>
-                <td>
-                  <div className="record">檢視</div>
-                </td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>50元折價卷</td>
-                <td>2020/01/15</td>
-                <td>2020/02/15</td>
-                <td className="fff-reward-used">已使用</td>
-                <td>
-                  <div className="record">檢視</div>
-                </td>
-              </tr>
-              <tr>
-                <td>5</td>
-                <td>50元折價卷</td>
-                <td>2020/01/15</td>
-                <td>2020/02/15</td>
-                <td className="fff-reward-used">已使用</td>
-                <td>
-                  <div className="record">檢視</div>
-                </td>
-              </tr>
+              {exchangeRecord.map((value, index) => (
+                <tr key={index + 1}>
+                  <td>{index + 1}</td>
+                  <td>{value.good_name}</td>
+                  <td>{value.event_time}</td>
+                  <td>{value.end_date}</td>
+                  {value.used_date == null ? (
+                    <td className="fff-reward-unuse">可使用</td>
+                  ) : (
+                    <td className="fff-reward-used">已使用</td>
+                  )}
+                  <td>
+                    <div className="record">檢視</div>
+                  </td>
+                </tr>
+              ))}
             </table>
           </div>
         </div>
@@ -85,115 +68,35 @@ function ExchangeRecord() {
         <div className="col-8 fff-no-mr-and-pad" style={{ height: ' 210px' }}>
           <div className="row d-flex align-items-center justify-content-around fff-no-mr-and-pad">
             <table id="fff-record-table">
-              <tr>
-                <td rowSpan="4">1</td>
-                <th>兌換項目</th>
-                <td>50元折價卷</td>
-                <td rowSpan="4">
-                  <div className="record">優惠條碼</div>
-                </td>
-              </tr>
-              <tr>
-                <th>兌換日期</th>
+              {exchangeRecord.map((value, index) => (
+                <>
+                  <tr>
+                    <td rowSpan="4">{index+1}</td>
+                    <th>兌換項目</th>
+                    <td>{value.good_name}</td>
+                    <td rowSpan="4">
+                      <div className="record">優惠條碼</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>兌換日期</th>
 
-                <td>2020/01/15</td>
-              </tr>
-              <tr>
-                <th>到期日期</th>
-                <td>2020/02/15</td>
-              </tr>
-              <tr>
-                <th>使用狀況</th>
-                <td className="fff-reward-used">已使用</td>
-              </tr>
-
-              <tr>
-                <td rowSpan="4">2</td>
-                <th>兌換項目</th>
-                <td>星巴克買一送一</td>
-                <td rowSpan="4">
-                  <div className="record">優惠條碼</div>
-                </td>
-              </tr>
-              <tr>
-                <th>兌換日期</th>
-
-                <td>2020/01/15</td>
-              </tr>
-              <tr>
-                <th>到期日期</th>
-                <td>2020/02/15</td>
-              </tr>
-              <tr>
-                <th>使用狀況</th>
-                <td className="fff-reward-unuse">可使用</td>
-              </tr>
-
-              <tr>
-                <td rowSpan="4">3</td>
-                <th>兌換項目</th>
-                <td>星巴克買一送一</td>
-                <td rowSpan="4">
-                  <div className="record">優惠條碼</div>
-                </td>
-              </tr>
-              <tr>
-                <th>兌換日期</th>
-
-                <td>2020/01/15</td>
-              </tr>
-              <tr>
-                <th>到期日期</th>
-                <td>2020/02/15</td>
-              </tr>
-              <tr>
-                <th>使用狀況</th>
-                <td className="fff-reward-unuse">可使用</td>
-              </tr>
-
-              <tr>
-                <td rowSpan="4">4</td>
-                <th>兌換項目</th>
-                <td>50元折價卷</td>
-                <td rowSpan="4">
-                  <div className="record">優惠條碼</div>
-                </td>
-              </tr>
-              <tr>
-                <th>兌換日期</th>
-
-                <td>2020/01/15</td>
-              </tr>
-              <tr>
-                <th>到期日期</th>
-                <td>2020/02/15</td>
-              </tr>
-              <tr>
-                <th>使用狀況</th>
-                <td className="fff-reward-used">已使用</td>
-              </tr>
-
-              <tr>
-                <td rowSpan="4">5</td>
-                <th>兌換項目</th>
-                <td>50元折價卷</td>
-                <td rowSpan="4">
-                  <div className="record">優惠條碼</div>
-                </td>
-              </tr>
-              <tr>
-                <th>兌換日期</th>
-
-                <td>2020/01/15</td>
-              </tr>
-              <tr>
-                <th>到期日期</th>
-                <td>2020/02/15</td>
-              </tr>
-              <tr>
-                <th>使用狀況</th>
-                <td className="fff-reward-used">已使用</td>
-              </tr>
+                    <td>{value.event_time}</td>
+                  </tr>
+                  <tr>
+                    <th>到期日期</th>
+                    <td>{value.end_date}</td>
+                  </tr>
+                  <tr>
+                    <th>使用狀況</th>
+                    {value.used_date == null ? (
+                    <td className="fff-reward-unuse">可使用</td>
+                  ) : (
+                    <td className="fff-reward-used">已使用</td>
+                  )}
+                  </tr>
+                </>
+              ))}
             </table>
           </div>
         </div>
