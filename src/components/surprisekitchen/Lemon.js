d10ec1439ab5528d655f8984e462ef1530fd0318
Lydia Liu <tin20630@gmail.com>
2021-02-22 21:06:12 +0800

    天RRRRRR天RRRRRR

--- src/components/surprisekitchen/Lemon.js
+++ src/components/surprisekitchen/Lemon.js
@@ -3,11 +3,12 @@ import React, { useEffect, useState, useCallback } from 'react'
 function Lemon() {
   return (
     <>
-      <div className="container lll-appoint col-sm-center">
+      <div className="container  col-sm-center">
         <div className="row justify-content-center">
-          <div className="col-8 lll-appoint-w100">
-            <div className="lll-lemon-position">
-              <div className="lll-lemon"></div>
+            <div className="d-flex lll-picture-position">
+              <div className="lll-lemon-position">
+                <div className="lll-lemon"></div>
+              
             </div>
           </div>
         </div>
