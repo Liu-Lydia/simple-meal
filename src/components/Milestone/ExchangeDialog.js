2c1a42a71e5691b44b76925a364616deda7c52cc
LeapHsu <ailiasdoll@gmail.com>
2021-02-23 18:11:27 +0800

    確認視窗

--- src/components/Milestone/ExchangeDialog.js
+++ src/components/Milestone/ExchangeDialog.js
@@ -14,6 +14,10 @@ function ExchangeDialog(props) {
   //按下確認 要去傳送資料
   const [commit, setCommit] = useState(false)
 
+  const [DBreturn, setDBReturn] = useState({})
+
+  const [returnMsgDialog, setReturnMsgDialog] = useState({ display: 'none' })
+
   const getPoint = async () => {
     const url = 'http://localhost:4000/milestone/getPoint?sid=1' //sid 要從session來
     await fetch(url, {
@@ -44,7 +48,8 @@ function ExchangeDialog(props) {
       //then 是會接前方拋出的結果
       .then((r) => r.json())
       .then((obj) => {
-        console.log("obj",obj)
+        console.log("obj", obj)
+        setDBReturn(obj)
       })
   }
 
@@ -73,101 +78,153 @@ function ExchangeDialog(props) {
     if (commit) {
       console.log('useEffect - commit')
       setExchange()
-      //彈出結果視窗 還沒做
+      //彈出結果視窗
+      setReturnMsgDialog({ display: 'flex' })
 
       setCommit(false)//復位 讓後面的兌換也能按
     }
   }, [commit])
 
   return (
-    <div className="fff-exchange" style={props.dialogStyle}>
-      <div className="fff-exchange-content-box">
-        <div className="row">
-          {/* 圖片放置位置 先占高 mobo關閉 */}
-
-          {/* 關閉頁面的Ｘ */}
-          <div
-            className=" justify-content-end col-12 "
-            style={{ marginBottom: '25px' }}
-          >
-            <h6 className="fff-no-mr-and-pad">
-              <i
-                className="fas fa-times aboutCloseBtn"
-                onClick={() => {
-                  props.setDialogStyle({ display: 'none' })
-                }}
-              ></i>
-            </h6>
-          </div>
-          <form id="dialog-form">
-            <div className="col fff-exchange-content ">
-              <div className="d-flex justify-content-between">
-                <span className="txt-h6">你點選了</span>
-                <span style={{ color: '#627E2A' }}>
-                  {props.detailContext.good_name}
-                </span>
-              </div>
-              <div className="d-flex justify-content-between">
-                <span className="txt-body" style={{ color: '#627E2A' }}>
-                  選擇數量
-                </span>
-                <select
-                  name="count"
-                  onChange={(event) => {
-                    setCount(event.target.value)
-                  }}
-                >
-                  {selectOption.map((value, index) => (
-                    <option key={index}>{index}</option>
-                  ))}
-                </select>
-              </div>
-              <div className="d-flex justify-content-between">
-                <span>花費點數</span>
-                <span>{count * props.detailContext.need_point}</span>
-              </div>
-              <div className="d-flex justify-content-between">
-                <span>剩餘點數</span>
-                <span>
-                  {totalPoint - count * props.detailContext.need_point}
-                </span>
-              </div>
-              <div>
-                <span className="txt-btn">
-                  兌換說明：
-                  {props.detailContext.good_subs}
-                </span>
-              </div>
-
-              <div>
-                <span className="txt-btn">已兌換便無法退還點數喔！</span>
-              </div>
-              <input
-                type="hidden"
-                name="good_ID"
-                value={props.detailContext.good_ID}
-              ></input>
-              <div
-                className="d-flex justify-content-center"
-                style={{ marginTop: '25px' }}
-              >
-                <button
-                  type="button"
-                  className="btn-green txt-btn aboutCloseBtn"
+    <>
+      {/* 結果視窗 */}
+      <div className="fff-exchange" style={returnMsgDialog}>
+        <div className="fff-exchange-content-box">
+          <div className="row">
+            {/* 關閉頁面的Ｘ */}
+            <div
+              className=" justify-content-end col-12 "
+              style={{ marginBottom: '25px' }}
+            >
+              <h6 className="fff-no-mr-and-pad">
+                <i
+                  className="fas fa-times aboutCloseBtn"
                   onClick={() => {
-                    props.setDialogStyle({ display: 'none' })
-                    setCommit(true)
+                    setReturnMsgDialog({ display: 'none' })
                   }}
-                  data-dismiss="modal"
-                >
-                  確定兌換
-                </button>
-              </div>
+                ></i>
+              </h6>
             </div>
-          </form>
+            <form id="dialog-form">
+              <div className="col fff-exchange-content ">
+
+                <div>
+                  <span className="txt-btn">{DBreturn.msg}</span>
+                </div>
+                <input
+                  type="hidden"
+                  name="good_ID"
+                  value={props.detailContext.good_ID}
+                ></input>
+                <div
+                  className="d-flex justify-content-center"
+                  style={{ marginTop: '25px' }}
+                >
+                  <button
+                    type="button"
+                    className="btn-green txt-btn aboutCloseBtn"
+                    onClick={() => {
+                      setReturnMsgDialog({ display: 'none' })
+                    }}
+                    data-dismiss="modal"
+                  >
+
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* 兌換視窗 */}
+      <div className="fff-exchange" style={props.dialogStyle}>
+        <div className="fff-exchange-content-box">
+          <div className="row">
+            {/* 關閉頁面的Ｘ */}
+            <div
+              className=" justify-content-end col-12 "
+              style={{ marginBottom: '25px' }}
+            >
+              <h6 className="fff-no-mr-and-pad">
+                <i
+                  className="fas fa-times aboutCloseBtn"
+                  onClick={() => {
+                    props.setDialogStyle({ display: 'none' })
+                  }}
+                ></i>
+              </h6>
+            </div>
+            <form id="dialog-form">
+              <div className="col fff-exchange-content ">
+                <div className="d-flex justify-content-between">
+                  <span className="txt-h6">你點選了</span>
+                  <span style={{ color: '#627E2A' }}>
+                    {props.detailContext.good_name}
+                  </span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <span className="txt-body" style={{ color: '#627E2A' }}>
+                    選擇數量
+                </span>
+                  <select
+                    name="count"
+                    onChange={(event) => {
+                      setCount(event.target.value)
+                    }}
+                  >
+                    {selectOption.map((value, index) => (
+                      <option key={index}>{index}</option>
+                    ))}
+                  </select>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <span>花費點數</span>
+                  <span>{count * props.detailContext.need_point}</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <span>剩餘點數</span>
+                  <span>
+                    {totalPoint - count * props.detailContext.need_point}
+                  </span>
+                </div>
+                <div>
+                  <span className="txt-btn">
+                    兌換說明：
+                  {props.detailContext.good_subs}
+                  </span>
+                </div>
+
+                <div>
+                  <span className="txt-btn">已兌換便無法退還點數喔！</span>
+                </div>
+                <input
+                  type="hidden"
+                  name="good_ID"
+                  value={props.detailContext.good_ID}
+                ></input>
+                <div
+                  className="d-flex justify-content-center"
+                  style={{ marginTop: '25px' }}
+                >
+                  <button
+                    type="button"
+                    className="btn-green txt-btn aboutCloseBtn"
+                    onClick={() => {
+                      props.setDialogStyle({ display: 'none' })
+                      setCommit(true)
+                    }}
+                    data-dismiss="modal"
+                  >
+                    確定兌換
+                </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
