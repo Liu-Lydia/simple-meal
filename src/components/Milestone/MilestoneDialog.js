eda48745bd036c4e60a1739ed4c7c26ccd9a8dbc
Leap Hsu <ailiasdoll@gmail.com>
2021-02-26 14:17:45 +0800

    改一些css

--- src/components/Milestone/MilestoneDialog.js
+++ src/components/Milestone/MilestoneDialog.js
@@ -3,15 +3,15 @@ import React, { props, useState, useEffect } from 'react'
 //detailStyle={detailStyle} setDetailStyle={setDetailStyle} detailContext={detailContext}
 function MilestoneDialog(props) {
   return (
-    <div className="fff-mshowto" style={props.detailStyle}>
+    <div className="fff-handmadepopup" style={props.detailStyle}>
       <div
         id="fff-ms-detail-content"
-        className="row fff-ms-detail-content"
-        onMouseLeave={() => {
+        className="fff-ms-detail-content"
+        onClick={() => {
           props.setDetailStyle({ visibility: 'hidden' })
         }}
       >
-        <div className="col justify-content-center">
+        <div className="row justify-content-end ">
           <div className="fff-ms-mobo justify-content-end col-12">
             {/* 關閉頁面的Ｘ */}
             <h6>
@@ -23,33 +23,46 @@ function MilestoneDialog(props) {
               ></i>
             </h6>
           </div>
-          <div className="fff-ms-web">
-            <img
-              className="col-4 align-center"
-              src={
-                'http://localhost:3015/img/milestonelist/' +
-                (props.detailContext.AddProgress >=
-                props.detailContext.progress_goal
-                  ? props.detailContext.finished_goal_pic
-                  : props.detailContext.unfinished_goal_pic)
-              }
-              alt=""
-            />
-            <div className="col-8">
-              <h3>{props.detailContext.stone_name}</h3>
-              <h5>{props.detailContext.TriggerSubs}</h5>
-              <p className="txt-h6" style={{ color: '#627e2a' }}>
-                {' '}
-                獎勵點數：{props.detailContext.reward_point}點
-              </p>
-              <p className="txt-h6">達成方式{props.detailContext.subs}</p>
-              <p className="txt-h6">
-                目前進度
-                {props.detailContext.AddProgress == null
-                  ? 0
-                  : props.detailContext.AddProgress}
-                /{props.detailContext.progress_goal}
-              </p>
+        </div>
+        <div className="row justify-content-center ">
+          <div className="fff-ms-web row justify-content-between">
+            <div className="col-4 align-self-center">
+              <img
+                style={{ width: '100%' }}
+                src={
+                  'http://localhost:3015/img/milestonelist/' +
+                  (props.detailContext.AddProgress >=
+                  props.detailContext.progress_goal
+                    ? props.detailContext.finished_goal_pic
+                    : props.detailContext.unfinished_goal_pic)
+                }
+                alt=""
+              />
+            </div>
+
+            <div className="col-6">
+              <div
+                className="row d-flex align-items-center"
+                style={{ height: '100%' }}
+              >
+                <div className="col">
+                  <h3 style={{ marginBottom: '30px' }}>
+                    {props.detailContext.stone_name}
+                  </h3>{' '}
+                  <p className="txt-h6" style={{ color: '#627e2a' }}>
+                    {' '}
+                    獎勵點數：{props.detailContext.reward_point}點
+                  </p>
+                  <p className="txt-h6">達成方式{props.detailContext.subs}</p>
+                  <p className="txt-h6">
+                    目前進度:{props.detailContext.TriggerSubs}
+                    {props.detailContext.AddProgress == null
+                      ? 0
+                      : props.detailContext.AddProgress}
+                    /{props.detailContext.progress_goal}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
           <div className="fff-ms-mobo row justify-content-center">
