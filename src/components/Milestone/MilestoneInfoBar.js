006061a6b1177ce1b94fa69d3aad5ffc8f48a1aa
Leap Hsu <ailiasdoll@gmail.com>
2021-03-02 05:43:32 +0800

    zzz

--- src/components/Milestone/MilestoneInfoBar.js
+++ src/components/Milestone/MilestoneInfoBar.js
@@ -94,7 +94,7 @@ function MilestoneInfoBar(props) {
                   // showDenyButton: true,
                   denyButtonText: '前往購物車',
                   // showCloseButton: true,
-                  backdrop: `rgba(0,0,0,.5)`,
+                  backdrop: `rgba(255,255,255,.5)`,
                 })
               }}
             >
@@ -193,7 +193,7 @@ function MilestoneInfoBar(props) {
                 // showDenyButton: true,
                 denyButtonText: '前往購物車',
                 // showCloseButton: true,
-                backdrop: `rgba(0,0,0,.5)`,
+                backdrop: `rgba(255,255,255,.5)`,
               })
             }}
           >
