258a83f2264b62de92ca730b39dc94285c30b8b9
LeapHsu <ailiasdoll@gmail.com>
2021-03-01 16:10:24 +0800

    監聽成就

---
+++ src/components/Milestone/MilestoneGoalLinstener.js
@@ -0,0 +1,11 @@
+import React, {useState } from 'react'
+  
+  //成就系統 檢查有沒有完成的成就
+  //當登入之後先檢查目前完成的成就並記錄下來
+  //每次畫面更新時檢查完成的成就數量是否不同
+  //數量不同要檢查是什麼更新 並呈現
+
+  //決定是否要開始監聽
+  export default function MilestoneGoalListener(){
+  const [milestoneListener,setMilestoneLinstener] = useState(false)
+  }
